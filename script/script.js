357ec53ce86ce2ac9223e9b3a0f84a1c3a412c07
NaoEhOMauricio <122880478+NaoEhOMauricio@users.noreply.github.com>
2023-03-01 19:14:56 -0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let novoIngrediente = document.getElementById('txtingrediente');
+let quantia = document.getElementById('quantidade');
+let preco = document.getElementById('preco')
+let Qemb = document.getElementById('quantiaembalagem')
+let tabela = document.getElementById('tabela')
+let btnAdd = document.getElementById('btnAdd')
+   
