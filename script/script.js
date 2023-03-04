077dbe49104c7e4f83e33572b43c3178b7a2a237
NaoEhOMauricio <122880478+NaoEhOMauricio@users.noreply.github.com>
2023-03-04 12:10:38 -0300

    Versão fuinha1.0

--- script/script.js
+++ script/script.js
@@ -7,90 +7,118 @@ let btnAdd = document.getElementById('btnAdd')
 let ModoDePreparo = document.getElementById('textarea');
 let btnAddObs = document.getElementById('btnAddObs')
 let c = 0;
+let n = 0
 let soma = 0;
 let AbIngrediente = document.getElementById('AbaIngrediente')
 let AbModo = document.getElementById('AbaModo')
 let SomaQuantia = document.getElementById('SomaQuantia')
+let TotalSoma = document.getElementById('TotalSoma');
+let ListaModo = document.getElementById('ListaModo')
 const ListaQuantia = [];
 const ListaResultado = [];
 
 
 //------------------------------------------------------------------------------------------------------
 function AdicionarIngrediente(){
-    //Caputra de valores digitados.
-    let ValorIngrediente = ingrediente.value;
-    let ValorQuantia = quantia.value;
-    let ValorPreço = preco.value;
-    let ValorQemb = Qemb.value;
-    let ValorResultado = ValorQuantia * ValorPreço / ValorQemb;
-  
-    if((ValorIngrediente !== "") && (ValorIngrediente !== 0) && (ValorIngrediente !== undefined)){
-      c++
-      let NovaLinha = `<tr id="${c}"> 
-        <td> ${ValorIngrediente}</td>
-        <td> ${ValorQuantia}gr </td>
-        <td> R$${ValorPreço} </td>
-        <td> ${ValorQemb}gr </td>
-        <td> ${ValorResultado} </td>
-        <td> <button onclick="deletar(${c})" class="delete">
-          <i class="material-symbols-outlined">
-              delete
-          </i></button> </td>
-        </tr>
-      `;
-  
-      ListaResultado.push(ValorResultado);
-      ListaQuantia.push(parseFloat(ValorQuantia));
-      tabela.innerHTML += NovaLinha;
-      atualizarSoma();
-      atualizarQuantia();
-  
-      ingrediente.value = "";
-      quantia.value = "";
-      preco.value = "";
-      Qemb.value = "";
-      ingrediente.focus(); 
-    }
+  // Captura de valores digitados.
+  let ValorIngrediente = ingrediente.value;
+  let ValorQuantia = quantia.value;
+  let ValorPreço = preco.value;
+  let ValorQemb = Qemb.value;
+  let ValorResultado = ValorQuantia * ValorPreço / ValorQemb;
+
+  // Verifica se algum campo está vazio
+  if (ValorIngrediente === "" || ValorQuantia === "" || ValorPreço === "" || ValorQemb === "") {
+    alert("Por favor, preencha todos os campos!");
+    return;
   }
 
+  c++;
+  let NovaLinha = `<tr id="${c}"> 
+    <td> ${ValorIngrediente}</td>
+    <td> ${ValorQuantia}gr </td>
+    <td> R$${ValorPreço} </td>
+    <td> ${ValorQemb}gr </td>
+    <td> R$${ValorResultado} </td>
+    <td> <button onclick="deletar(${c})" class="delete">
+      <i class="material-symbols-outlined">
+          delete
+      </i></button> </td>
+    </tr>`;
+
+  ListaResultado.push(ValorResultado);
+  ListaQuantia.push(parseFloat(ValorQuantia));
+  tabela.innerHTML += NovaLinha;
+  
+  atualizarQuantia();
+  atualizarValor();
+
+
+
+  // ingrediente.value = "";
+  // quantia.value = "";
+  // preco.value = "";
+  // Qemb.value = "";
+  // ingrediente.focus(); 
+}
+
+Qemb.addEventListener("keydown", function(event) {
+  // verifica se a tecla pressionada foi Enter
+  if (event.keyCode === 13) {
+    // chama a função AdicionarIngrediente()
+    AdicionarIngrediente();
+  }
+});
 // ====-------------------------------------------------------------------------------------------------------------------------------------------------------=
-function adicionarComentario(){
-    let Observacao = ModoDePreparo.value
-    let n = 0
-    if((Observacao !== 0 )&&(Observacao !== null) && (Observacao != "")){
+function adicionarComentario() {
+  let Observacao = ModoDePreparo.value;
+  
+  if (Observacao.trim() === "") {
+    alert("O campo de Modo de Preparo está vazio. Por favor, adicione um Modo de Preparo válido.");
+    return;
+  }
+
+  if ((Observacao !== 0) && (Observacao !== null) && (Observacao != "")) {
+
+    n++
+
+    let novoComentario = `
+    
+        <li id="cm-${n}" colspan="5">${Observacao}
+        <button onclick="deletarComentario('cm-${n}')" class="delete">
+            <i class="material-symbols-outlined">
+              delete
+            </i>
+          </button>
+        </li>
         
-        n++
-   
-    let novocomentario = `  <tr id="${n}"> <td colspan="5"> ${Observacao} </td>
-    <td id ="${n}"> <button onclick="deletarCm(${n})" class="delete">
-    <i class="material-symbols-outlined">
-        delete
-    </i></button> </td>
-    </tr>
+      
     `
 
-        tabela.innerHTML += novocomentario;
+    ListaModo.innerHTML += novoComentario;
 
-        ModoDePreparo.value = ""
-        ModoDePreparo.focus()
+    ModoDePreparo.value = ""
+    ModoDePreparo.focus()
+  }
 }
-
-}
+ModoDePreparo.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    adicionarComentario();
+  }
+});
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 function deletar(id){
-    var tarefa = document.getElementById(id);
-    const valorResultado = parseFloat(tarefa.children[4].textContent);
-    const ValorQuantia = parseFloat(tarefa.children[1].textContent);
-    tarefa.remove();
-    subtrairArray(valorResultado);
-    subtrairArrayQuantia(ValorQuantia)
-    subtrairArrayQuantia()
-    atualizarSoma()
-    atualizarQuantia()
+  var tarefa = document.getElementById(id);
+  const ValorRes = parseFloat(tarefa.children[1].textContent);
+  const ValorQuantia = parseFloat(tarefa.children[1].textContent);
+  tarefa.remove();
+  subtrairArrayQuantia(ValorQuantia)
+  subtrairArrayValor(ValorRes)
+  atualizarValor()
+  atualizarQuantia()
 }
-
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function trocarAba(numero) {
@@ -107,16 +135,26 @@ function trocarAba(numero) {
   }
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function atualizarSoma() {
-    const soma = ListaResultado.reduce((total, numero) => total + numero, 0);
-    const elementoSoma = document.getElementById("TotalSoma");
-    elementoSoma.textContent = soma.toFixed(2);
-    
-    // Verifica se a lista está vazia
-    if (ListaResultado.length === 0) {
-      elementoSoma.textContent = 0;
-    }
+
+
+
+
+
+function atualizarValor() {
+  const soma = ListaResultado.reduce((total, numero) => total + numero, 0);
+  const elementoSoma = document.getElementById("TotalSoma");
+  elementoSoma.textContent = soma.toFixed(1) + "gr";
+  
+  // Verifica se a lista está vazia
+  if (ListaResultado.length === 0) {
+    elementoSoma.textContent = 0;
   }
+}
+
+
+
+
+
   function atualizarQuantia() {
     const soma = ListaQuantia.reduce((total, numero) => total + numero, 0);
     const elementoSoma = document.getElementById("TotalQuantia");
@@ -129,17 +167,19 @@ function atualizarSoma() {
   }
 
 //   ---------------------------------------------------------------------------------------------------------------------------------------------
-function subtrairArray(valor) {
-    const index = ListaResultado.indexOf(valor);
-    if (index > -1) {
-      ListaResultado.splice(index, 1);
-    }
-    const resultado = ListaResultado.reduce((acumulador, valorAtual) => {
-      return acumulador - valorAtual;
-    }, 0);
-    atualizarSoma();
-    return resultado;
+function subtrairArrayValor(valor) {
+  const index = ListaResultado.indexOf(valor);
+  if (index > -1) {
+    ListaResultado.splice(index, 1);
+  }
+  const resultado = ListaResultado.reduce((acumulador, valorAtual) => {
+    return acumulador - valorAtual;
+  }, 0);
+  atualizarValor();
+  return resultado;
 }
+
+
 function subtrairArrayQuantia(valor) {
     const index = ListaQuantia.indexOf(valor);
     if (index > -1) {
@@ -152,10 +192,9 @@ function subtrairArrayQuantia(valor) {
     return resultado;
 }
 
-function deletarCm(id){
-    var tarefa = document.getElementById(id);
-    tarefa.remove();
-   
+function deletarComentario(id) {
+  var comentario = document.getElementById(id);
+  comentario.remove();
 }
 function imprimir() {
     window.print();
